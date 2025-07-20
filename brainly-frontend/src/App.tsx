839b54f5ac29efa03c0e5c160c52811d73c3fe41
Sid <siddharthmethiya180@gmail.com>
2025-07-20 17:13:05 +0530

    Completed button comp

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import { Button } from './components/Button'
 import './index.css'
+import { PlusIcon } from './components/icons/PlusIcon'
+import { ShareIcon } from './components/icons/ShareIcon'
 
 function App() {
    return (
       <div className="">
-         <Button variant="primary" text="Hello" size="lg" />
+         <Button
+            variant="primary"
+            text="Add Content"
+            startIcon={<PlusIcon />}
+         />
          <br />
          <br />
-         <Button variant="secondary" text="Hello" size="lg" />
+         <Button
+            variant="secondary"
+            text="Share Brain"
+            startIcon={<ShareIcon />}
+         />
       </div>
    )
 }
--- brainly-frontend/src/components/Button.tsx
+++ brainly-frontend/src/components/Button.tsx
@@ -3,16 +3,21 @@ import type { ReactElement } from 'react'
 type Variant = 'primary' | 'secondary'
 interface ButtonProps {
    variant: Variant
-   size: 'sm' | 'md' | 'lg'
    text: string
    startIcon?: ReactElement
-   endIcon?: ReactElement
    onClick?: () => void
 }
 const variantType: Record<Variant, string> = {
-   primary: 'bg-purple-600',
-   secondary: 'bg-purple-300 text-purple-600',
+   primary:
+      'bg-purple-600 text-white font-normal text-lg px-4 py-2 rounded text-center mt-10 rounded-sm flex items-center',
+   secondary:
+      'bg-purple-200 text-purple-600 font-normal text-lg px-4 py-2 rounded text-center mt-10 rounded-sm flex items-center',
 }
-export const Button = (props: ButtonProps) => {
-   return <button className={variantType[props.variant]}>Click me</button>
+export const Button = ({ variant, text, startIcon }: ButtonProps) => {
+   return (
+      <button className={variantType[variant]}>
+         <div className="pr-2">{startIcon}</div>
+         <span>{text}</span>
+      </button>
+   )
 }
