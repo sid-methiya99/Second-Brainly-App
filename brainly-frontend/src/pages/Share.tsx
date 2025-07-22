8bf37369d4279c56d11a2782715610535cc27015
Sid <siddharthmethiya180@gmail.com>
2025-07-22 23:38:07 +0530

    Improved share view

--- brainly-frontend/src/pages/Share.tsx
+++ brainly-frontend/src/pages/Share.tsx
@@ -13,26 +13,27 @@ export const Share = () => {
    return (
       <div className="pt-10">
          <div className="w-full">
-            <div className="mx-10 ">
-               <div className="flex justify-between ">
-                  <div className="flex justify-center items-center">
-                     <h1 className="mx-10 font-bold text-3xl">{username}</h1>
-                  </div>
+            <div className="mx-auto max-w-7xl px-4">
+               <div className="flex justify-center items-center mb-8">
+                  <h1 className="font-bold text-3xl text-center">
+                     {username} Brain
+                  </h1>
                </div>
-            </div>
-            <div className="grid grid-cols-3 px-10">
-               {brainContents?.map(
-                  ({ _id, link, type, title, tags, date }: CardProps) => (
-                     <ShareCard
-                        _id={_id}
-                        type={type}
-                        link={link}
-                        title={title}
-                        tags={tags}
-                        date={date}
-                     />
-                  )
-               )}
+               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                  {brainContents?.map(
+                     ({ _id, link, type, title, tags, date }: CardProps) => (
+                        <ShareCard
+                           key={_id}
+                           _id={_id}
+                           type={type}
+                           link={link}
+                           title={title}
+                           tags={tags}
+                           date={date}
+                        />
+                     )
+                  )}
+               </div>
             </div>
          </div>
       </div>
